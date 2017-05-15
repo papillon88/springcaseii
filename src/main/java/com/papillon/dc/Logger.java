3175c3998fbd1360edad4b87658e667e1230bcc2
papillon <devopreet.ch@gmail.com>
2017-05-15 15:37:47 -0500

    *autowire using annotations *xml schema added context

--- src/main/java/com/papillon/dc/Logger.java
+++ src/main/java/com/papillon/dc/Logger.java
@@ -1,5 +1,7 @@
 package com.papillon.dc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.File;
 
 /**
@@ -7,16 +9,33 @@ import java.io.File;
  */
 public class Logger {
 
+    /*autowiring can be mixed and matched too.
+    like autowire one property using constructor and the
+            other using private instance fields
+            also its not necessary to have setters and getters
+    for autowire.*/
+
+    //@Autowired
     private ConsoleWriter consoleWriter;
+    //@Autowired
     private FileWriter fileWriter;
 
+    //@Autowired
+    public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+        this.consoleWriter = consoleWriter;
+        this.fileWriter = fileWriter;
+    }
+
+    @Autowired
     public void setConsoleWriter(ConsoleWriter consoleWriter) {
         this.consoleWriter = consoleWriter;
     }
 
+    @Autowired
     public void setFileWriter(FileWriter fileWriter) {
         this.fileWriter = fileWriter;
     }
+
 
     public void writeFile(String text){
         fileWriter.write(text);
